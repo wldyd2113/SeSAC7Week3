d1164c31d539bc588aa9390dccf5ea832db69c89
jiyong <wldyd2113@gmail.com>
2025-07-11 11:14:41 +0900

    0과 1추가

--- SeSAC7Week3/ViewController.swift
+++ SeSAC7Week3/ViewController.swift
@@ -12,10 +12,12 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         print(1)
+        print(0)
         print(2)
         print(3)
         print(4)
         print(5)
+        print(6)
     }
 
 
