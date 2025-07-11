f59e4f152c7c103b9c775e2b459f943e40125434
jiyong <wldyd2113@gmail.com>
2025-07-11 11:06:17 +0900

    Initial Commit

---
+++ SeSAC7Week3/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  SeSAC7Week3
+//
+//  Created by 차지용 on 7/11/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print(1)
+        print(2)
+        print(3)
+    }
+
+
+}
+
