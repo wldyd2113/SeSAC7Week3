b99d770e081ede4445fdb99b54d445265063faf9
jiyong <wldyd2113@gmail.com>
2025-07-11 11:17:04 +0900

    버튼 추가

--- SeSAC7Week3/ViewController.swift
+++ SeSAC7Week3/ViewController.swift
@@ -8,6 +8,13 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let buttongGit: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("버튼", for: .normal)
+        
+        return button
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,6 +28,8 @@ class ViewController: UIViewController {
         
         //print 나 파일 추가 등 코드 작성하면서
         //commit message 를 3개 정도 남겨보기
+        print(9)
+        view.addSubview(buttongGit)
     }
 
 
