7332054f7cfcfdd23517e40e138b95928fc7f914
jiyong <wldyd2113@gmail.com>
2025-07-11 11:32:34 +0900

    MovieTableViecll 생성

---
+++ SeSAC7Week3/MovieTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  MovieTableViewCell.swift
+//  SeSAC7Week3
+//
+//  Created by 차지용 on 7/11/25.
+//
+
+import UIKit
+
+class MovieTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
